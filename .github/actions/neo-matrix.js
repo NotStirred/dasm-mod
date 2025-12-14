7c61eb852059a0f9d528aef8616c4fd284c96819
Tom Martin <tom.martin1239@gmail.com>
2025-12-14 17:11:32 +0000

    fix ci

--- .github/actions/neo-matrix.js
+++ .github/actions/neo-matrix.js
@@ -1,24 +1,16 @@
 import { XMLParser } from "fast-xml-parser";
 
-try {
-    const response = await fetch("https://maven.neoforged.net/releases/net/neoforged/neoforge/maven-metadata.xml");
-    if (!response.ok) {
-        // noinspection ExceptionCaughtLocallyJS
-        throw new Error(`Neoforge versions response status: ${response.status}`);
-    }
-
-    const parser = new XMLParser();
-    let data = parser.parse(await response.text());
-
-    process.stdout.write(JSON.stringify(data))
-
-    const output = Array.from(data.metadata.versioning.versions)
-        .map(node => node.version)
-        .filter(version => !version.includes("w")) // filter out weird snapshot versions like 0.25w14craftmine.3-beta
-
-    process.stdout.write(output)
-
-} catch (e) {
-    console.error(e);
-    process.exit(1)
+const response = await fetch("https://maven.neoforged.net/releases/net/neoforged/neoforge/maven-metadata.xml");
+if (!response.ok) {
+    // noinspection ExceptionCaughtLocallyJS
+    throw new Error(`Neoforge versions response status: ${response.status}`);
 }
+
+const parser = new XMLParser();
+let data = parser.parse(await response.text());
+
+const output = data.metadata.versioning.versions.version
+    .filter(version => !version.includes("w")) // filter out weird snapshot versions like 0.25w14craftmine.3-beta
+
+process.stdout.write(JSON.stringify(output))
+
