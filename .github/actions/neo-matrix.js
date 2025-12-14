d841d0389a02367db358322b7e6717b84e5c4615
Tom Martin <tom.martin1239@gmail.com>
2025-12-14 17:42:48 +0000

    fix ci

--- .github/actions/neo-matrix.js
+++ .github/actions/neo-matrix.js
@@ -29,5 +29,9 @@ let data = parser.parse(text);
 const output = data.metadata.versioning.versions.version
     .filter(version => !version.includes("w")) // filter out weird snapshot versions like 0.25w14craftmine.3-beta
 
-process.stdout.write("'" + JSON.stringify(output) + "'")
+process.stdout.write("\'" +
+    JSON.stringify(output)
+        .replaceAll("\"", "\\\"")
+    + "\'"
+)
 
