60c530426295cd4c73a1b54f9926941a941f894f
Tom Martin <tom.martin1239@gmail.com>
2025-12-14 16:59:26 +0000

    fix ci

--- .github/actions/neo-matrix.js
+++ .github/actions/neo-matrix.js
@@ -3,13 +3,14 @@ import { XMLParser } from "fast-xml-parser";
 try {
     const response = await fetch("https://maven.neoforged.net/releases/net/neoforged/neoforge/maven-metadata.xml");
     if (!response.ok) {
+        // noinspection ExceptionCaughtLocallyJS
         throw new Error(`Neoforge versions response status: ${response.status}`);
     }
 
     const parser = new XMLParser();
     let data = parser.parse(await response.text());
 
-    process.stdout.write(data)
+    process.stdout.write(JSON.stringify(data))
 
     const output = data.metadata.versioning.versions
         .map(node => node.version)
