3a1ca0d278422fd4c4ada38fc142e932a1ad76aa
Tom Martin <tom.martin1239@gmail.com>
2025-12-14 17:38:46 +0000

    fix ci

--- .github/actions/neo-matrix.js
+++ .github/actions/neo-matrix.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { XMLParser } from "fast-xml-parser";
 
 const response = await fetch("https://maven.neoforged.net/releases/net/neoforged/neoforge/maven-metadata.xml");
@@ -6,8 +7,24 @@ if (!response.ok) {
     throw new Error(`Neoforge versions response status: ${response.status}`);
 }
 
+const text = `<?xml version="1.0" encoding="UTF-8"?>
+<metadata>
+  <groupId>net.neoforged</groupId>
+  <artifactId>neoforge</artifactId>
+  <versioning>
+    <latest>21.11.6-beta</latest>
+    <release>21.11.6-beta</release>
+    <versions>
+      <version>21.11.5-beta</version>
+      <version>21.11.6-beta</version>
+    </versions>
+    <lastUpdated>20251212004616</lastUpdated>
+  </versioning>
+</metadata>
+`;
+
 const parser = new XMLParser();
-let data = parser.parse(await response.text());
+let data = parser.parse(text);
 
 const output = data.metadata.versioning.versions.version
     .filter(version => !version.includes("w")) // filter out weird snapshot versions like 0.25w14craftmine.3-beta
