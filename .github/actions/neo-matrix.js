46806ebff73ec73bb5e10c37d3e46f0a4150c3a9
Tom Martin <tom.martin1239@gmail.com>
2025-12-14 18:36:53 +0000

    fix ci

--- .github/actions/neo-matrix.js
+++ .github/actions/neo-matrix.js
@@ -7,25 +7,9 @@ if (!response.ok) {
     throw new Error(`Neoforge versions response status: ${response.status}`);
 }
 
-const text = `<?xml version="1.0" encoding="UTF-8"?>
-<metadata>
-  <groupId>net.neoforged</groupId>
-  <artifactId>neoforge</artifactId>
-  <versioning>
-    <latest>21.11.6-beta</latest>
-    <release>21.11.6-beta</release>
-    <versions>
-      <version>21.11.5-beta</version>
-      <version>21.11.6-beta</version>
-    </versions>
-    <lastUpdated>20251212004616</lastUpdated>
-  </versioning>
-</metadata>
-`;
-
 
 const parser = new XMLParser();
-let data = parser.parse(text);
+let data = parser.parse(await response.text());
 
 const output = data.metadata.versioning.versions.version
     .filter(version => !version.includes("w")) // filter out weird snapshot versions like 0.25w14craftmine.3-beta
